1f2c65253d8bd3443e20dfffd72f5dd7d480500f
liuzheng <609722369@qq.com>
2020-07-29 15:04:58 +0800

    完善jetpack

---
+++ app/src/main/java/com/panda/jetpackdemo/motionlayout/MotionlayoutActivity.kt
@@ -0,0 +1,13 @@
+package com.panda.jetpackdemo.motionlayout
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.panda.jetpackdemo.R
+
+class MotionlayoutActivity :AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_motionlayout)
+    }
+}
---
+++ app/src/main/java/com/panda/jetpackdemo/notification/NotificationActivity.kt
@@ -0,0 +1,61 @@
+package com.panda.jetpackdemo.notification
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.panda.jetpackdemo.R
+import com.panda.jetpackdemo.setting.SettingActivity
+
+class NotificationActivity : AppCompatActivity() {
+
+    private val CHANNEL_ID = "jimu_channelID"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "mychannel"
+            val descriptionText = "for test"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun showNotification(){
+        val intent = Intent(this, SettingActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("My notification")
+            .setContentText("Hello World!")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            // Set the intent that will fire when the user taps the notification
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        with(NotificationManagerCompat.from(this)) {
+            notify(1, builder.build())
+        }
+
+    }
+
+}
